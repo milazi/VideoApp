26a863486c3cf08fea38c1e7159990bfe5bb1261
Ahamad Milazi <ahamad.milazi@technobraingroup.com>
2022-06-04 12:39:03 +0200

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer>{
+                new Customer() { Id=1, Name ="John Smith"},
+                new Customer() { Id=2, Name ="Mary Williams"}
+            };
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int id)
+        {
+            var customer = (from cust in customers where cust.Id.Equals(id) select cust ).FirstOrDefault();
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = new List<Movie> { 
+                new Movie { Name = "Shrek!" },
+                new Movie { Name = "Abominable Snowman"}
+            };
+            return View(movies);
+        }
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer() { Name ="Customer 1"},
+                new Customer() { Name ="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {   
+                Movie = movie,
+                Customers = customers
+            };
+            //ViewBag.Movie = movie;
+            //  return View(movie);
+
+            return View(viewModel);
+            // return Content("Hello World");
+
+            // return HttpNotFound();
+
+            // return new EmptyResult();
+
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+        
+        //MOVIES
+        /*
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if(String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex={0}&sortby={1}", pageIndex, sortBy));
+        }
+        */
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
