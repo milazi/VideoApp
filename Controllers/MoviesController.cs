a24e1b5b2a8a1f5b74d610b5f65ac3ba80612946
Ahamad Milazi <ahamad.milazi@technobraingroup.com>
2022-06-05 12:25:26 +0200

    feat: - added extra columns and genre to movie - added Genre model - added Movie detail view

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,18 +5,34 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController() {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ActionResult Index()
         {
-            var movies = new List<Movie> { 
-                new Movie { Name = "Shrek!" },
-                new Movie { Name = "Abominable Snowman"}
-            };
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
         // GET: Movies/Random
         public ActionResult Random()
