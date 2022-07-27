17e7fcaa8d4e4dcf04b4f4ab915d517a44b546a7
Ahamad Milazi <ahamad.milazi@technobraingroup.com>
2022-07-27 20:19:29 +0200

    feat: added-migration-for-rental-and-driving-license

---
+++ Dtos/RentalDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class RentalDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public List<int> MovieIds { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime? DateReturned { get; set; }
+    }
+}
---
+++ Migrations/202206131730404_NewRental.cs
@@ -0,0 +1,35 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewRental : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Rentals",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        MovieId = c.Int(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .ForeignKey("dbo.Movies", t => t.MovieId, cascadeDelete: true)
+                .Index(t => t.MovieId)
+                .Index(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Rentals", "MovieId", "dbo.Movies");
+            DropForeignKey("dbo.Rentals", "CustomerId", "dbo.Customers");
+            DropIndex("dbo.Rentals", new[] { "CustomerId" });
+            DropIndex("dbo.Rentals", new[] { "MovieId" });
+            DropTable("dbo.Rentals");
+        }
+    }
+}
